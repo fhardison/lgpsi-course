7d8e487c565359ab8a6b3cce7b9ffcadc7ae2e04
Fletcher Hardison <fhardison@@gmail.com>
2025-03-05 11:57:40 -0500

    adding cpt 5

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,4 @@ uv run text-loom.py ../src/chapter_01.md ../src/paratext.txt | pandoc -s -o ../d
 uv run text-loom.py ../src/chapter_02.md ../src/paratext.txt | pandoc -s -o ../docs/chapter_02.html -f markdown+raw_html --metadata title="LGPSI 2" --css simple-v1.css
 uv run text-loom.py ../src/chapter_03.md ../src/paratext.txt | pandoc -s -o ../docs/chapter_03.html -f markdown+raw_html --metadata title="LGPSI 3" --css simple-v1.css
 uv run text-loom.py ../src/chapter_04.md ../src/paratext.txt | pandoc -s -o ../docs/chapter_04.html -f markdown+raw_html --metadata title="LGPSI 4" --css simple-v1.css
+uv run text-loom.py ../src/chapter_05.md ../src/paratext.txt | pandoc -s -o ../docs/chapter_05.html -f markdown+raw_html --metadata title="LGPSI 5" --css simple-v1.css
